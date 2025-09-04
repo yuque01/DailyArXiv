471ee3a58c78562a9094fbeedd03ae7be3b1c1df
Zhaohong Liu <37680723+pstAmbition@users.noreply.github.com>
2025-09-04 14:28:20 +0800

    Reorder keywords list in main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ with open("README.md", "r") as f:
     # if last_update_date == current_date:
         # sys.exit("Already updated today!")
 
-keywords = ["Agent", "Misinformation Detection", "LLM", "Representation Learning", "Multimodal Learning"] # TODO add more keywords
+keywords = ["Multimodal Learning", "Representation Learning", "Misinformation Detection", "LLM", "Agent"] # TODO add more keywords
 
 max_result = 100 # maximum query results from arXiv API for each keyword
 issues_result = 15 # maximum papers to be included in the issue
